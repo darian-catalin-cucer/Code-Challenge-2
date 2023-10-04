d1c2e8a51ebba543a6f3aa0313c8a10a9018d2cc
Darian-Cătălin Cucer <cucerdariancatalin@gmail.com>
2023-10-04 14:50:16 +0300

    1st run

---
+++ app/src/main/java/com/codechallenge2/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.codechallenge2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var inputTextView: TextView
+    private lateinit var resultTextView: TextView
+
+    private var currentInput = ""
+    private var currentOperator = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        inputTextView = findViewById(R.id.inputTextView)
+        resultTextView = findViewById(R.id.resultTextView)
+    }
+
+    fun onDigitClick(view: View) {
+        val digit = (view as Button).text.toString()
+        currentInput += digit
+        updateInputText()
+    }
+
+    fun onOperatorClick(view: View) {
+        currentOperator = (view as Button).text.toString()
+        currentInput += currentOperator
+        updateInputText()
+    }
+
+    fun onEqualsClick(view: View) {
+        try {
+            val result = evaluateExpression()
+            resultTextView.text = result.toString()
+        } catch (e: Exception) {
+            resultTextView.text = "Error"
+        }
+    }
+
+    fun onClearClick(view: View) {
+        currentInput = ""
+        currentOperator = ""
+        inputTextView.text = ""
+        resultTextView.text = getString(R.string.error_message)
+    }
+
+
+    private fun updateInputText() {
+        inputTextView.text = currentInput
+    }
+
+    private fun evaluateExpression(): Double {
+        val parts = currentInput.split(currentOperator)
+        val operand1 = parts[0].toDouble()
+        val operand2 = parts[1].toDouble()
+
+        return when (currentOperator) {
+            "+" -> operand1 + operand2
+            "-" -> operand1 - operand2
+            "*" -> operand1 * operand2
+            "/" -> {
+                if (operand2 == 0.0) {
+                    throw ArithmeticException("Division by zero")
+                }
+                operand1 / operand2
+            }
+            else -> throw IllegalArgumentException("Invalid operator")
+        }
+    }
+}
